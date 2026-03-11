713ed6d8b07fc4e078894e9109cb9f2331ea8f90
Kent Francis Genilo <kegenilo@up.edu.ph>
2026-03-11 11:04:57 +0800

    feat: integrate save gate

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -15,6 +15,7 @@ import { PlayerProvider } from './contexts/PlayerContext.tsx';
 import { EquipmentProvider } from './contexts/EquipmentContext.tsx';
 import { ItemProvider } from './contexts/ItemContext.tsx';
 import { EnemyProvider } from './contexts/EnemyContext.tsx';
+import GameSaveGate from './contexts/GameSaveGate.tsx';
 
 const RootRedirect = () => {
   const token = localStorage.getItem('game_token');
@@ -28,25 +29,27 @@ const App: React.FC = () => {
         <PlayerProvider>
           <ItemProvider>
             <EnemyProvider>
-              <Routes>
-                {/* Public Routes */}
-                <Route path="/" element={<RootRedirect />} />
-                <Route path="/signup" element={<Signup />} />
-                <Route path="/login" element={<Login />} />
+              <GameSaveGate>
+                <Routes>
+                  {/* Public Routes */}
+                  <Route path="/" element={<RootRedirect />} />
+                  <Route path="/signup" element={<Signup />} />
+                  <Route path="/login" element={<Login />} />
 
-                {/* Private Game Routes */}
-                
-                <Route element={<ProtectedRoute />}>
-                  <Route element={<GameLayout />}>
-                    <Route path="/dashboard" element={<Dashboard />} />
-                    <Route path="/inventory" element={<Inventory />} />
-                    <Route path="/character" element={<Character />} />
-                    <Route path="/shop" element={<Shop />} />
+                  {/* Private Game Routes */}
+                  
+                  <Route element={<ProtectedRoute />}>
+                    <Route element={<GameLayout />}>
+                      <Route path="/dashboard" element={<Dashboard />} />
+                      <Route path="/inventory" element={<Inventory />} />
+                      <Route path="/character" element={<Character />} />
+                      <Route path="/shop" element={<Shop />} />
+                    </Route>
                   </Route>
-                </Route>
-                {/* Catch-all redirect */}
-                <Route path="*" element={<Navigate to="/login" />} />
-              </Routes>
+                  {/* Catch-all redirect */}
+                  <Route path="*" element={<Navigate to="/login" />} />
+                </Routes>
+              </GameSaveGate>
             </EnemyProvider>
           </ItemProvider>
         </PlayerProvider>
